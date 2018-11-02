2fd496c61f616b01b9deef4832bdf938f9032c31
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-02 11:44:51 +0100

    cleanup the namespace before running the tests

--- .openshiftio/test.sh
+++ .openshiftio/test.sh
@@ -8,6 +8,25 @@ if [ ! -d ".openshiftio" ]; then
   exit 1
 fi
 
+# cleanup
+oc delete build --all
+oc delete bc --all
+oc delete dc --all
+oc delete deploy --all
+oc delete is --all
+oc delete istag --all
+oc delete isimage --all
+oc delete job --all
+oc delete po --all
+oc delete rc --all
+oc delete rs --all
+oc delete statefulsets --all
+oc delete secrets --all
+oc delete configmap --all
+oc delete services --all
+oc delete routes --all
+oc delete template --all
+
 # Deploy the templates and required resources
 oc apply -f name-service/.openshiftio/application.yaml
 oc apply -f greeting-service/.openshiftio/application.yaml
